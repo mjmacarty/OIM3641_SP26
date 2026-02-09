fc47a413e964710353b88d97d9847e7fe2000ab3
mjmacarty <matt.macarty@gmail.com>
2026-02-09 16:01:40 -0500

    class files

--- 06-ui_starter.py
+++ 06-ui_starter.py
@@ -4,6 +4,8 @@ import plotly.express as px
 import streamlit as st
 import yfinance as yf
 
+st.set_page_config(layout='wide', page_title="Stock Analyzer")
+
 # CONSTANTS
 END = date.today()
 START = END - timedelta(365)
@@ -19,3 +21,86 @@ def get_stock_data(ticker, start, end):
         return data, f"Successfully loaded data for {ticker}"
     except Exception as e:
         return None, f"Error {e}"
+
+# sidebar
+st.sidebar.title("🗠 Inputs")
+ticker = st.sidebar.text_input("Stock symbol", value="AAPL").upper()
+col1, col2 = st.sidebar.columns(2)
+start = col1.date_input("Start Date", value=START)
+end = col2.date_input("End Date", value=END)
+moving_average = st.sidebar.slider("Moving Average Window",
+                                   min_value=10,
+                                   max_value=100,
+                                   value=50,
+                                   step=5)
+run = st.sidebar.button("Run Analysis", type='primary')
+
+#main UI
+st.title("Stock Analyzer")
+
+if run:
+    with st.spinner(f"Fetching {ticker} data..."):
+        df, message = get_stock_data(ticker, start, end)
+    if df is not None:
+        st.sidebar.success(message)
+    else:
+        st.sidebar.error(message)
+    df['MA'] = df['Close'].rolling(window=moving_average).mean()
+    df['pct_change'] = df['Close'].pct_change() * 100
+
+    # output
+    tab1, tab2, tab3 = st.tabs(['📉 Chart', ' 📊Statistics', ' 🗄️Raw Data'])
+    with tab1:
+        st.subheader(f"{ticker} High Level Analysis")
+        col1, col2, col3 = st.columns(3)
+        col1.metric(f'Latest Price', f'{df['Close'].iloc[-1]:.2f}')
+        col2.metric(f'Cum Change',
+                    f'{((df['Close'].iloc[-1]/df['Close'].iloc[0]) -1) * 100:.2f}')
+        col3.metric(f'Trading Days', f'{len(df)}')
+        figure = px.line(df, y= ['Close', 'MA'])
+        figure.update_layout(hovermode='x unified')
+        with st.spinner("Creating chart"):
+            st.plotly_chart(figure, use_container_width=True)
+
+    with tab2:
+        st.subheader("Summary Statistics")
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write("**Daily Change Statistics**")
+            summary = df['pct_change'].describe()
+            st.dataframe(summary)
+        with col2:
+            price_stats = pd.DataFrame(
+                {'Metric': ['High', 'Low', 'Mean', 'Volatility'],
+                 'Values': [
+                     f"{df["Close"].max()}",
+                     f"{df["Close"].min()}",
+                     f"{df["Close"].mean()}",
+                     f"{df["Close"].std()}"
+                 ]
+                }
+            )
+            st.dataframe(price_stats)
+
+    with tab3:
+        st.subheader("Raw Data")
+        st.dataframe(df.tail(10))
+        csv = df.to_csv()
+        st.download_button(
+                           label="Download Data",
+                           data = csv,
+                           file_name = f"{ticker}_{start}_{end}.csv",
+                           mime="text/csv"
+                            )
+
+
+
+
+
+
+
+
+
+
+
+
