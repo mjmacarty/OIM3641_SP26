02fb364a5f4b5a2c43eb749f72390763a1c7e2fa
mjmacarty <matt.macarty@gmail.com>
2026-02-25 10:01:59 -0500

    class files

--- 10-bare-bones.rag.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
-from llama_index.embeddings.google_genai import GoogleGenAIEmbedding
-from llama_index.llms.google_genai import GoogleGenAI
-import streamlit st
-
-
-
